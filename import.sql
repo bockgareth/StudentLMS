641e3f92078c845b33bf296f5b988e7c5042f2bb
Gareth <bockgareth@gmail.com>
2018-05-20 21:44:29 +0200

    Code refactoring

--- import.sql
+++ import.sql
@@ -126,7 +126,8 @@ CREATE TABLE `security` (
   `id` int(11) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
